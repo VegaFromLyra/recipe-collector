1d8e8b3bd51317796b3ebd7c7ce435aa5d4b5ed7
ashab <asha.ab@gmail.com>
2020-02-11 11:18:57 -0500

    Validate recipe name and cuisine and give better form to request body

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,16 @@
 class Recipe < ApplicationRecord
   has_many :steps
+
+  module Cuisine
+    INDIAN = 'Indian'
+    AMERICAN = 'American'
+    ITALIAN = 'Italian'
+
+    def self.all
+      constants.each { |c| const_get(c) }
+    end
+  end
+
+  validates :name, presence: true
+  validates :cuisine, inclusion: { in: Cuisine.all, message: "%{value} is not a valid cuisine" }
 end
--- test/controllers/api/v1/recipes_controller_test.rb
+++ test/controllers/api/v1/recipes_controller_test.rb
@@ -2,7 +2,18 @@ require 'test_helper'
 
 class Api::V1::RecipesControllerTest < ActionDispatch::IntegrationTest
   test "creates a recipe" do
-    post api_v1_recipes_url, params: { recipe: { title: "alu paratha", description: "alu and paratha == alu paratha" } }
+    steps = [
+      {
+        text: "Get 2 cups of atta",
+      },
+      {
+        text: "Boil potatoes",
+      },
+    ]
+
+    params = { recipe: { title: "Alu Paratha", steps: steps } }
+
+    post api_v1_recipes_url, params: params
 
     assert_response :success
   end
