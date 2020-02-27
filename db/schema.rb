0ac70f68e48ca02a608efb68678e9a4b6000752a
ashab <asha.ab@gmail.com>
2020-02-27 15:23:17 -0500

    Add is active flag to recipe so we can delete the garbage ones

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_20_142411) do
+ActiveRecord::Schema.define(version: 2020_02_27_202125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_02_20_142411) do
     t.string "external_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "is_active", default: true, null: false
   end
 
   create_table "steps", force: :cascade do |t|
