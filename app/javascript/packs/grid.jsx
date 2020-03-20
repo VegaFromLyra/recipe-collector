e1ac3f2ca5c6ef0f1b4dce7ef57b2d7bd7f0be49
ashab <asha.ab@gmail.com>
2020-03-20 17:31:26 -0400

    Move grid into it's own file
    * Using named exports instead of default just for funsies
    * Manually importing react in each component file till I  figure out how
    to load react globally

---
+++ app/javascript/packs/grid.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Grid extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1> Recipes are coming...</h1>
+      </div>
+    );
+  }
+}
+
+export { Grid }
