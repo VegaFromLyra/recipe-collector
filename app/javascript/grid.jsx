f82ad003e71f5027fc14e846baace02399faae90
ashab <asha.ab@gmail.com>
2020-03-25 17:04:07 -0400

    Beginnings of a grid layout

--- app/javascript/grid.jsx
+++ app/javascript/grid.jsx
@@ -4,7 +4,21 @@ class Grid extends React.Component {
   render() {
     return (
       <div className="text-center">
-        <h1>Recipes are coming..</h1>
+        <div className="flex flex-wrap mb-4">
+          <div className="sm:w-full xl:w-1/3 mb-4 bg-gray-500">Recipe 1</div>
+          <div className="sm:w-full xl:w-1/3 mb-4 bg-gray-500">Recipe 2</div>
+          <div className="sm:w-full xl:w-1/3 mb-4 bg-gray-500">Recipe 3</div>
+        </div>
+        <div className="flex flex-wrap mb-4">
+          <div className="sm:w-full xl:w-1/3 mb-4 bg-gray-500">Recipe 4</div>
+          <div className="sm:w-full xl:w-1/3 mb-4 bg-gray-500">Recipe 5</div>
+          <div className="sm:w-full xl:w-1/3 mb-4 bg-gray-500">Recipe 6</div>
+        </div>
+        <div className="flex flex-wrap mb-4">
+          <div className="sm:w-full xl:w-1/3 mb-4 bg-gray-500">Recipe 7</div>
+          <div className="sm:w-full xl:w-1/3 mb-4 bg-gray-500">Recipe 8</div>
+          <div className="sm:w-full xl:w-1/3 mb-4 bg-gray-500">Recipe 9</div>
+        </div>
       </div>
     );
   }
