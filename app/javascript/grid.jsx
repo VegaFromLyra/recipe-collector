9189b12dd413e3f5ce697cc18007da7726664b7d
ashab <asha.ab@gmail.com>
2020-03-25 16:31:51 -0400

    Switch to using styles from tailwind

--- app/javascript/grid.jsx
+++ app/javascript/grid.jsx
@@ -1,30 +1,10 @@
 import Square from './square'
 
 class Grid extends React.Component {
-  renderSquare() {
-    return (
-      <Square />
-    );
-  }
-
   render() {
     return (
-      <div className="grid">
-        <div className="board-row">
-          { this.renderSquare() }
-          { this.renderSquare() }
-          { this.renderSquare() }
-        </div>
-        <div className="board-row">
-          { this.renderSquare() }
-          { this.renderSquare() }
-          { this.renderSquare() }
-        </div>
-        <div className="board-row">
-          { this.renderSquare() }
-          { this.renderSquare() }
-          { this.renderSquare() }
-        </div>
+      <div className="text-center">
+        <h1>Recipes are coming..</h1>
       </div>
     );
   }
--- app/javascript/packs/main.jsx
+++ app/javascript/packs/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import Grid from '../grid'
-import '../styles/main.css'
+import '../css/main.css'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
