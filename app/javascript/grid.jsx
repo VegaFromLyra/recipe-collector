9eb736d06a44bcbd1d916f0e15159bb7dfc69785
ashab <asha.ab@gmail.com>
2020-03-26 16:16:33 -0400

    First pass at the grid with hardcoded data

--- app/javascript/grid.jsx
+++ app/javascript/grid.jsx
@@ -1,19 +1,24 @@
 import Square from './square'
-import placeholder from 'placeholder.png'
 
 class Grid extends React.Component {
   render() {
     return (
-      <div className="text-center container">
+      <div className="py-5">
         <div className="flex flex-wrap mb-4">
-          <div className="sm:w-full xl:w-1/3 mb-4">
-            <img src={placeholder}/>
+          <div className="w-full h-64 xl:w-1/3 mb-4 border-2 rounded-lg bg-red-300">
+            <div className="p-3">
+              <p className="text-white font-bold text-6xl xl:text-xl">Semiya upma </p>
+            </div>
           </div>
-          <div className="sm:w-full xl:w-1/3 mb-4">
-            <img src={placeholder}/>
+          <div className="w-full h-64 xl:w-1/3 mb-4 border-2 rounded-lg bg-red-300">
+            <div className="p-3">
+              <p className="text-white font-bold text-6xl xl:text-xl">Alu paratha </p>
+            </div>
           </div>
-          <div className="sm:w-full xl:w-1/3 mb-4">
-            <img src={placeholder}/>
+          <div className="w-full h-64 xl:w-1/3 mb-4 border-2 rounded-lg bg-red-300">
+            <div className="p-3">
+              <p className="text-white font-bold text-6xl xl:text-xl">Dosa batter </p>
+            </div>
           </div>
         </div>
       </div>
