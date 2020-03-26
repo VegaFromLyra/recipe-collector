42012cfff1b9651d72927b138647895037367b9c
ashab <asha.ab@gmail.com>
2020-03-26 16:35:09 -0400

    Extract html into square, so much nicer

--- app/javascript/grid.jsx
+++ app/javascript/grid.jsx
@@ -5,21 +5,9 @@ class Grid extends React.Component {
     return (
       <div className="py-5">
         <div className="flex flex-wrap mb-4">
-          <div className="w-full h-64 xl:w-1/3 mb-4 border-2 rounded-lg bg-red-300">
-            <div className="p-3">
-              <p className="text-white font-bold text-6xl xl:text-xl">Semiya upma </p>
-            </div>
-          </div>
-          <div className="w-full h-64 xl:w-1/3 mb-4 border-2 rounded-lg bg-red-300">
-            <div className="p-3">
-              <p className="text-white font-bold text-6xl xl:text-xl">Alu paratha </p>
-            </div>
-          </div>
-          <div className="w-full h-64 xl:w-1/3 mb-4 border-2 rounded-lg bg-red-300">
-            <div className="p-3">
-              <p className="text-white font-bold text-6xl xl:text-xl">Dosa batter </p>
-            </div>
-          </div>
+          <Square name="Semiya upma"/>
+          <Square name="Green chutney"/>
+          <Square name="Dosa batter"/>
         </div>
       </div>
     );
--- app/javascript/square.jsx
+++ app/javascript/square.jsx
@@ -1,7 +1,10 @@
 function Square(props) {
   return (
-    <button className="square">
-    </button>
+    <div className="w-full h-64 xl:w-1/3 mb-4 border-2 rounded-lg bg-red-300">
+      <div className="p-3">
+        <p className="text-white font-bold text-5xl xl:text-xl">{props.name}</p>
+      </div>
+    </div>
   );
 }
 
