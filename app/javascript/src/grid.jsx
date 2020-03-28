9ef8ad0d46d665ec0776438348d5d7ba9b0abd69
ashab <asha.ab@gmail.com>
2020-03-27 23:20:37 -0400

    Keep track of success, error and loading states in the grid

--- app/javascript/src/grid.jsx
+++ app/javascript/src/grid.jsx
@@ -3,26 +3,58 @@ import Square from "./square"
 const axios = require("axios")
 
 class Grid extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: null,
+      isLoaded: false,
+      recipes: [],
+    }
+  }
+
   componentDidMount() {
     axios.get("/api/v1/recipes?page=1&limit=3")
-      .then(function(response) {
-        console.log(response);
+      .then((response) => {
+        this.setState({
+          isLoaded: true,
+          recipes: response.data
+        });
       })
-      .catch(function (error) {
-        console.log(error);
+      .catch((error) => {
+        this.setState({
+          isLoaded: true,
+          error: error
+        });
       })
   }
 
   render() {
-    return (
-      <div className="py-5">
-        <div className="flex flex-wrap mb-4">
-          <Square name="Semiya upma"/>
-          <Square name="Green chutney"/>
-          <Square name="Dosa batter"/>
+    const { error, isLoaded, recipes } = this.state
+
+    if (error) {
+      return (
+        <div className="py-5">
+          Error: { error.message }
         </div>
-      </div>
-    );
+      );
+    } else if (!isLoaded) {
+      return (
+        <div className="py-5">
+          Loading..
+        </div>
+      );
+    } else {
+      return (
+        <div className="py-5">
+          <div className="flex flex-wrap mb-4">
+            <Square name="Semiya upma"/>
+            <Square name="Green chutney"/>
+            <Square name="Dosa batter"/>
+          </div>
+        </div>
+      );
+    }
   }
 }
 
